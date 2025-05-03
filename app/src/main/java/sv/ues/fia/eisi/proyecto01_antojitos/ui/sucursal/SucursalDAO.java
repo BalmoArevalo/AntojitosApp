755ce1e5e117c03f7134cce64baab9591a113bd1
Bmo09 <AO22002@ues.edu.sv>
2025-05-02 21:41:06 -0600

    :zap: Logica para implementar spinners y guardar sucursal

--- app/src/main/java/sv/ues/fia/eisi/proyecto01_antojitos/ui/sucursal/SucursalDAO.java
+++ app/src/main/java/sv/ues/fia/eisi/proyecto01_antojitos/ui/sucursal/SucursalDAO.java
@@ -15,93 +15,81 @@ public class SucursalDAO {
         this.db = db;
     }
 
-    // Insertar una nueva sucursal
-    public long insertar(Sucursal sucursal) {
+    public long insertar(Sucursal s) {
         ContentValues values = new ContentValues();
-        values.put("ID_SUCURSAL", sucursal.getIdSucursal());
-        values.put("ID_DEPARTAMENTO", sucursal.getIdDepartamento());
-        values.put("ID_MUNICIPIO", sucursal.getIdMunicipio());
-        values.put("ID_DISTRITO", sucursal.getIdDistrito());
-        values.put("ID_USUARIO", sucursal.getIdUsuario());
-        values.put("NOMBRE_SUCURSAL", sucursal.getNombreSucursal());
-        values.put("DIRECCION_SUCURSAL", sucursal.getDireccionSucursal());
-        values.put("TELEFONO_SUCURSAL", sucursal.getTelefonoSucursal());
-        values.put("HORARIO_APERTURA_SUCURSAL", sucursal.getHorarioApertura());
-        values.put("HORARIO_CIERRE_SUCURSAL", sucursal.getHorarioCierre());
+        values.put("ID_DEPARTAMENTO", s.getIdDepartamento());
+        values.put("ID_MUNICIPIO", s.getIdMunicipio());
+        values.put("ID_DISTRITO", s.getIdDistrito());
+        values.put("ID_USUARIO", s.getIdUsuario()); // Asumido fijo
+        values.put("NOMBRE_SUCURSAL", s.getNombreSucursal());
+        values.put("DIRECCION_SUCURSAL", s.getDireccionSucursal());
+        values.put("TELEFONO_SUCURSAL", s.getTelefonoSucursal());
+        values.put("HORARIO_APERTURA_SUCURSAL", s.getHorarioApertura());
+        values.put("HORARIO_CIERRE_SUCURSAL", s.getHorarioCierre());
 
         return db.insert("SUCURSAL", null, values);
     }
 
-    // Consultar por ID
-    public Sucursal consultarPorId(int idSucursal) {
-        Cursor cursor = db.query("SUCURSAL", null, "ID_SUCURSAL = ?",
-                new String[]{String.valueOf(idSucursal)},
-                null, null, null);
+    public Sucursal obtenerPorId(int idSucursal) {
+        Cursor c = db.rawQuery("SELECT * FROM SUCURSAL WHERE ID_SUCURSAL = ?", new String[]{String.valueOf(idSucursal)});
+        Sucursal s = null;
 
-        if (cursor.moveToFirst()) {
-            Sucursal sucursal = new Sucursal();
-            sucursal.setIdSucursal(cursor.getInt(0));
-            sucursal.setIdDepartamento(cursor.getInt(1));
-            sucursal.setIdMunicipio(cursor.getInt(2));
-            sucursal.setIdDistrito(cursor.getInt(3));
-            sucursal.setIdUsuario(cursor.getInt(4));
-            sucursal.setNombreSucursal(cursor.getString(5));
-            sucursal.setDireccionSucursal(cursor.getString(6));
-            sucursal.setTelefonoSucursal(cursor.getString(7));
-            sucursal.setHorarioApertura(cursor.getString(8));
-            sucursal.setHorarioCierre(cursor.getString(9));
-            cursor.close();
-            return sucursal;
-        } else {
-            cursor.close();
-            return null;
+        if (c.moveToFirst()) {
+            s = new Sucursal();
+            s.setIdSucursal(c.getInt(0));
+            s.setIdDepartamento(c.getInt(1));
+            s.setIdMunicipio(c.getInt(2));
+            s.setIdDistrito(c.getInt(3));
+            s.setIdUsuario(c.getInt(4));
+            s.setNombreSucursal(c.getString(5));
+            s.setDireccionSucursal(c.getString(6));
+            s.setTelefonoSucursal(c.getString(7));
+            s.setHorarioApertura(c.getString(8));
+            s.setHorarioCierre(c.getString(9));
         }
+        c.close();
+        return s;
     }
 
-    // Actualizar sucursal existente
-    public int actualizar(Sucursal sucursal) {
-        ContentValues values = new ContentValues();
-        values.put("ID_DEPARTAMENTO", sucursal.getIdDepartamento());
-        values.put("ID_MUNICIPIO", sucursal.getIdMunicipio());
-        values.put("ID_DISTRITO", sucursal.getIdDistrito());
-        values.put("ID_USUARIO", sucursal.getIdUsuario());
-        values.put("NOMBRE_SUCURSAL", sucursal.getNombreSucursal());
-        values.put("DIRECCION_SUCURSAL", sucursal.getDireccionSucursal());
-        values.put("TELEFONO_SUCURSAL", sucursal.getTelefonoSucursal());
-        values.put("HORARIO_APERTURA_SUCURSAL", sucursal.getHorarioApertura());
-        values.put("HORARIO_CIERRE_SUCURSAL", sucursal.getHorarioCierre());
-
-        return db.update("SUCURSAL", values, "ID_SUCURSAL = ?",
-                new String[]{String.valueOf(sucursal.getIdSucursal())});
-    }
-
-    // Eliminar sucursal
-    public int eliminar(int idSucursal) {
-        return db.delete("SUCURSAL", "ID_SUCURSAL = ?", new String[]{String.valueOf(idSucursal)});
-    }
-
-    // Obtener todas las sucursales (opcional)
     public List<Sucursal> obtenerTodas() {
         List<Sucursal> lista = new ArrayList<>();
-        Cursor cursor = db.rawQuery("SELECT * FROM SUCURSAL", null);
+        Cursor c = db.rawQuery("SELECT * FROM SUCURSAL", null);
 
-        if (cursor.moveToFirst()) {
+        if (c.moveToFirst()) {
             do {
-                Sucursal sucursal = new Sucursal();
-                sucursal.setIdSucursal(cursor.getInt(0));
-                sucursal.setIdDepartamento(cursor.getInt(1));
-                sucursal.setIdMunicipio(cursor.getInt(2));
-                sucursal.setIdDistrito(cursor.getInt(3));
-                sucursal.setIdUsuario(cursor.getInt(4));
-                sucursal.setNombreSucursal(cursor.getString(5));
-                sucursal.setDireccionSucursal(cursor.getString(6));
-                sucursal.setTelefonoSucursal(cursor.getString(7));
-                sucursal.setHorarioApertura(cursor.getString(8));
-                sucursal.setHorarioCierre(cursor.getString(9));
-                lista.add(sucursal);
-            } while (cursor.moveToNext());
+                Sucursal s = new Sucursal();
+                s.setIdSucursal(c.getInt(0));
+                s.setIdDepartamento(c.getInt(1));
+                s.setIdMunicipio(c.getInt(2));
+                s.setIdDistrito(c.getInt(3));
+                s.setIdUsuario(c.getInt(4));
+                s.setNombreSucursal(c.getString(5));
+                s.setDireccionSucursal(c.getString(6));
+                s.setTelefonoSucursal(c.getString(7));
+                s.setHorarioApertura(c.getString(8));
+                s.setHorarioCierre(c.getString(9));
+                lista.add(s);
+            } while (c.moveToNext());
         }
-        cursor.close();
+        c.close();
         return lista;
+    }
+
+    public int actualizar(Sucursal s) {
+        ContentValues values = new ContentValues();
+        values.put("ID_DEPARTAMENTO", s.getIdDepartamento());
+        values.put("ID_MUNICIPIO", s.getIdMunicipio());
+        values.put("ID_DISTRITO", s.getIdDistrito());
+        values.put("NOMBRE_SUCURSAL", s.getNombreSucursal());
+        values.put("DIRECCION_SUCURSAL", s.getDireccionSucursal());
+        values.put("TELEFONO_SUCURSAL", s.getTelefonoSucursal());
+        values.put("HORARIO_APERTURA_SUCURSAL", s.getHorarioApertura());
+        values.put("HORARIO_CIERRE_SUCURSAL", s.getHorarioCierre());
+
+        return db.update("SUCURSAL", values, "ID_SUCURSAL = ?", new String[]{String.valueOf(s.getIdSucursal())});
+    }
+
+    public int eliminar(int idSucursal) {
+        return db.delete("SUCURSAL", "ID_SUCURSAL = ?", new String[]{String.valueOf(idSucursal)});
     }
 }
