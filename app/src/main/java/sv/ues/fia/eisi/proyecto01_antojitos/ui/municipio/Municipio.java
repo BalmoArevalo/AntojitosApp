a8ada508cb4383d290798d22cf768632d3b37b50
KikeHerrera <149346367+KikeHerrera@users.noreply.github.com>
2025-05-06 00:43:32 -0600

    Trabajando Municipio

--- app/src/main/java/sv/ues/fia/eisi/proyecto01_antojitos/ui/municipio/Municipio.java
+++ app/src/main/java/sv/ues/fia/eisi/proyecto01_antojitos/ui/municipio/Municipio.java
@@ -1,4 +1,44 @@
 package sv.ues.fia.eisi.proyecto01_antojitos.ui.municipio;
 
 public class Municipio {
+    private int idMunicipio;
+    private String nombre;
+    private int idDepartamento;
+
+    public Municipio() {}
+
+    public Municipio(int idMunicipio, String nombre, int idDepartamento) {
+        this.idMunicipio = idMunicipio;
+        this.nombre = nombre;
+        this.idDepartamento = idDepartamento;
+    }
+
+    public int getIdMunicipio() {
+        return idMunicipio;
+    }
+
+    public void setIdMunicipio(int idMunicipio) {
+        this.idMunicipio = idMunicipio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getIdDepartamento() {
+        return idDepartamento;
+    }
+
+    public void setIdDepartamento(int idDepartamento) {
+        this.idDepartamento = idDepartamento;
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
 }
--- app/src/main/java/sv/ues/fia/eisi/proyecto01_antojitos/ui/municipio/MunicipioDAO.java
+++ app/src/main/java/sv/ues/fia/eisi/proyecto01_antojitos/ui/municipio/MunicipioDAO.java
@@ -1,4 +1,69 @@
 package sv.ues.fia.eisi.proyecto01_antojitos.ui.municipio;
+package sv.ues.fia.eisi.proyecto01_antojitos.db;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import sv.ues.fia.eisi.proyecto01_antojitos.modelos.Municipio;
 
 public class MunicipioDAO {
+    private SQLiteDatabase db;
+
+    public MunicipioDAO(SQLiteDatabase db) {
+        this.db = db;
+    }
+
+    public long insertar(Municipio municipio) {
+        ContentValues valores = new ContentValues();
+        valores.put("idMunicipio", municipio.getIdMunicipio());
+        valores.put("nombre", municipio.getNombre());
+        valores.put("idDepartamento", municipio.getIdDepartamento());
+        return db.insert("Municipio", null, valores);
+    }
+
+    public int actualizar(Municipio municipio) {
+        ContentValues valores = new ContentValues();
+        valores.put("nombre", municipio.getNombre());
+        valores.put("idDepartamento", municipio.getIdDepartamento());
+        return db.update("Municipio", valores, "idMunicipio = ?", new String[]{String.valueOf(municipio.getIdMunicipio())});
+    }
+
+    public int eliminar(int idMunicipio) {
+        return db.delete("Municipio", "idMunicipio = ?", new String[]{String.valueOf(idMunicipio)});
+    }
+
+    public Municipio consultarPorId(int idMunicipio) {
+        Cursor cursor = db.rawQuery("SELECT * FROM Municipio WHERE idMunicipio = ?", new String[]{String.valueOf(idMunicipio)});
+        if (cursor.moveToFirst()) {
+            Municipio municipio = new Municipio(
+                    cursor.getInt(0),
+                    cursor.getString(1),
+                    cursor.getInt(2)
+            );
+            cursor.close();
+            return municipio;
+        }
+        cursor.close();
+        return null;
+    }
+
+    public List<Municipio> obtenerTodos() {
+        List<Municipio> lista = new ArrayList<>();
+        Cursor cursor = db.rawQuery("SELECT * FROM Municipio", null);
+        while (cursor.moveToNext()) {
+            Municipio municipio = new Municipio(
+                    cursor.getInt(0),
+                    cursor.getString(1),
+                    cursor.getInt(2)
+            );
+            lista.add(municipio);
+        }
+        cursor.close();
+        return lista;
+    }
 }
+
