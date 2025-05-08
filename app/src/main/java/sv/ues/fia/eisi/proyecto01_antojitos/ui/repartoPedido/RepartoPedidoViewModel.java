b569e0e2d50ab820b73bcbb0139dbc8034e71c22
CarlosRauda64 <rauda.carlos6432@gmail.com>
2025-05-08 17:29:18 -0600

    ✅  Reparto pedido añadido

---
+++ app/src/main/java/sv/ues/fia/eisi/proyecto01_antojitos/ui/repartoPedido/RepartoPedidoViewModel.java
@@ -0,0 +1,7 @@
+package sv.ues.fia.eisi.proyecto01_antojitos.ui.repartoPedido;
+
+import androidx.lifecycle.ViewModel;
+
+public class RepartoPedidoViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
